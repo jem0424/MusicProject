ac4bb46ec53137886b10cbed39dfec6c1fba39c4
jem0424 <49951966+jem0424@users.noreply.github.com>
2019-10-15 13:19:55 -0400

    Django framework for web app

---
+++ mysite/flexpage/models.py
@@ -0,0 +1,35 @@
+"""flex page"""
+
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
+
+class FlexPage(Page):
+
+    template = "flex/flex_page.html"
+
+    #@todo add streamfields
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock())
+        ],
+        null=True,
+        blank=True,
+    )
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
+        ]
+
+    class Meta:
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
+# Create your models here.
